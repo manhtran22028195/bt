06b4eb4d44f6c639b3a0120249a6a51826924a58
manhtran22028195 <125729112+manhtran22028195@users.noreply.github.com>
2023-03-13 17:14:32 +0700

    a

--- bt06/partition.cpp
+++ bt06/partition.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector <int> num;
-void pre(int n,int k){
-    if(n==0){
-        for(int i=0;i<num.size();i++)
-            cout<< num[i]<<" ";
-        cout<< endl;
+int num[1005];
+void pre(int n, int k, int h) {
+    if (n == 0) {
+        for (int i = 0; i < h; i++)
+            cout << num[i] << " ";
+        cout << endl;
         return;
     }
-    if(n<0){
+    if (n < 0) {
         return;
     }
-    for(int i=k;i>=1;i--){
-        num.push_back(i);
-        k=i;
-        pre(n-i,k);
-        num.pop_back();
+    for (int i = k; i >= 1; i--) {
+        num[h] = i;
+        k = i;
+        pre(n - i, k, h + 1);
     }
 }
 
 int main()
 {
     int n;
-    cin>> n;
-    pre(n,n);
+    cin >> n;
+    pre(n, n, 0);
 }
